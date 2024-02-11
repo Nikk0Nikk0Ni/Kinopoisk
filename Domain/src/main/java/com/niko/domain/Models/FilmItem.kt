d9ec7a7446c9631dc54b07a74c9be379c4f9fc19
Nikko Grigalashvili <nickokar@mail.ru>
2024-02-11 16:43:55 +0300

    Last version of app

--- Domain/src/main/java/com/niko/domain/Models/FilmItem.kt
+++ Domain/src/main/java/com/niko/domain/Models/FilmItem.kt
@@ -4,21 +4,13 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity("Films")
 data class FilmItem(
-    @PrimaryKey
     val kinopoiskId : Int,
-    @ColumnInfo
     val nameRu : String,
-    @ColumnInfo
     val posterUrl : String,
-    @ColumnInfo
     val year : Int,
-    @ColumnInfo
     val type : String,
-    @ColumnInfo
     val genres : List<Genres>,
-    @ColumnInfo
     var isFavourite : Boolean,
     var id : Int = UNDEFIND_ID
 )
